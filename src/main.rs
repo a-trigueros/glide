8f34d3abca743e0c8d7556fe621cf174544cad4f
Alexandre Trigueros <80602661+a-trigueros@users.noreply.github.com>
2026-06-20 20:53:54 +0200

    refactor: try to expose ui features properly

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use crossterm::event::{Event, KeyCode};
+use std::io;
+
+use crossterm::event::{Event, KeyCode, KeyEvent};
 use ratatui::{
     DefaultTerminal, Frame,
     layout::{Constraint, Layout},
@@ -11,24 +13,76 @@ use tui_input::{Input, backend::crossterm::EventHandler};
 struct App {
     input: Input,
     messages: Vec<String>,
+    need_exit: bool,
 }
 
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Error {
+    NoMatchingCommand,
+    Io {
+        #[allow(dead_code)]
+        source: io::Error,
+    },
+    Eyre {
+        #[allow(dead_code)]
+        source: color_eyre::eyre::Error,
+    },
+}
+
+impl From<io::Error> for Error {
+    fn from(value: io::Error) -> Self {
+        Error::Io { source: value }
+    }
+}
+
+impl From<color_eyre::eyre::Error> for Error {
+    fn from(value: color_eyre::eyre::Error) -> Self {
+        Error::Eyre { source: value }
+    }
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
 impl App {
-    fn run(&mut self, terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<()> {
         loop {
+            if self.need_exit {
+                return Ok(());
+            }
+
             terminal.draw(|frame| self.render(frame))?;
             let event = crossterm::event::read()?;
-            if let Event::Key(key) = event {
-                if key.code == KeyCode::Enter {
-                    let typed = self.input.value().to_string();
-                    self.input.reset();
-                    if typed.eq("/quit") {
-                        return Ok(());
-                    } else {
-                        self.messages.push(typed.to_string());
+            if let Event::Key(KeyEvent {
+                code,
+                modifiers: _,
+                kind: _,
+                state: _,
+            }) = event
+            {
+                match code {
+                    KeyCode::Enter => {
+                        let typed = self.input.value().to_string();
+                        self.input.reset();
+                        self.handle_command(&typed)
+                            // or_else validate fuzzyfinding
+                            .or_else(|_| -> Result<()> {
+                                self.messages.push(typed);
+                                Ok(())
+                            })?;
                     }
-                } else {
-                    self.input.handle_event(&event);
+                    KeyCode::Char('@') => {
+                        self.messages
+                            .push("missing feature: fzf in files".to_string());
+                        self.input.handle_event(&event);
+                    }
+                    KeyCode::Char('/') => {
+                        self.messages.push("missing feature: suggest commands (native and user added) if there are some".to_string());
+                        self.input.handle_event(&event);
+                    }
+                    _ => {
+                        self.input.handle_event(&event);
+                    }
                 }
             }
         }
@@ -65,9 +119,19 @@ impl App {
     fn render_help_area(&self, frame: &mut Frame, area: ratatui::prelude::Rect) {
         frame.render_widget("help area", area);
     }
+
+    fn handle_command(&mut self, command: &str) -> Result<()> {
+        match command {
+            "/quit" => {
+                self.need_exit = true;
+                Ok(())
+            }
+            _ => Err(Error::NoMatchingCommand),
+        }
+    }
 }
 
-fn main() -> color_eyre::Result<()> {
+fn main() -> Result<()> {
     color_eyre::install()?;
     let mut terminal = ratatui::init();
     App::default().run(&mut terminal)?;
